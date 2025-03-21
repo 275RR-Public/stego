275d176410d035440067ddcc2aff060496a87d34
275RR-Public <105179611+275RR-Public@users.noreply.github.com>
2025-03-21 15:11:09 -0500

    added sidebar

--- app.py
+++ app.py
@@ -13,20 +13,21 @@ def get_gcs_client():
 client = get_gcs_client()
 bucket_name = st.secrets["app_data"]["bucket_name"]
 
-# Function to display images (accessible to everyone)
-def display_images():
-    image_urls = bucket.get_image_urls(client, bucket_name)
-    if image_urls:
-        for url in image_urls:
-            st.image(url)
+# Sidebar for authentication
+with st.sidebar:
+    st.title("Login")
+    st.write("Anyone can view uploaded images.")
+    if st.experimental_user.is_logged_in:
+        st.write(f"Welcome, **{st.experimental_user.name}**!")
+        if st.button("Logout"):
+            st.logout()
     else:
-        st.info("No images uploaded yet.")
+        st.write("Login to create and upload images using Steganography.")
+        if st.button("Login with Google"):
+            st.login("google")
 
-# Check if the user is logged in
+# If user logged in then show Upload
 if st.experimental_user.is_logged_in:
-    st.write(f"Welcome, {st.experimental_user.name}!")
-    
-    # Show upload widget only to logged-in users
     uploaded_file = st.file_uploader("Upload an image", type=["jpg", "png", "jpeg"])
     if uploaded_file is not None:
         file_extension = os.path.splitext(uploaded_file.name)[1]
@@ -34,15 +35,16 @@ if st.experimental_user.is_logged_in:
         public_url = bucket.upload_image(client, bucket_name, uploaded_file, unique_filename)
         bucket.add_image_url(client, bucket_name, public_url)
         st.success("Image uploaded successfully!")
-    
-    # Show logout button
-    if st.button("Logout"):
-        st.logout()
-else:
-    # Show login button and message for non-logged-in users
-    if st.button("Login with Google"):
-        st.login("google")
-    st.write("Please log in to upload images.")
 
-# Always display images, regardless of login status
-display_images()
+# Display images (accessible to everyone)
+st.title("Image Steganography")
+image_urls = bucket.get_image_urls(client, bucket_name)
+if image_urls:
+    with st.container(border=True):
+        col1, col2, col3 = st.columns(3)
+        for i, url in enumerate(image_urls):
+            if i % 3 == 0: col1.image(url)
+            elif i % 3 == 1: col2.image(url)
+            elif i % 3 == 2: col3.image(url)
+else:
+    st.info("No images uploaded yet.")
